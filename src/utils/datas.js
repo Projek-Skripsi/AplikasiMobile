93948c6b4aa113871aa280be7ed329d473fbbffa
mhdAzhar <azhartk20@gmail.com>
2023-05-08 17:33:10 +0700

    fix: setup project

---
+++ src/utils/datas.js
@@ -0,0 +1,31 @@
+const keranjang = [
+  {
+    id: 1,
+    name: 'Lorem Ipsum',
+    image: 'https://dikemas.com/uploads/2022/04/Banana-Juara_Duo-Kuning_Logoo-720x432.jpg',
+    harga: 20000,
+    toko: {
+      name: 'Toko Bumi Pertiwi'
+    }
+  },
+  {
+    id: 1,
+    name: 'Lorem Ipsum',
+    image: 'https://dikemas.com/uploads/2022/04/Banana-Juara_Duo-Kuning_Logoo-720x432.jpg',
+    harga: 20000,
+    toko: {
+      name: 'Toko Bumi Pertiwi'
+    }
+  },
+  {
+    id: 1,
+    name: 'Lorem Ipsum',
+    image: 'https://dikemas.com/uploads/2022/04/Banana-Juara_Duo-Kuning_Logoo-720x432.jpg',
+    harga: 20000,
+    toko: {
+      name: 'Toko Bumi Pertiwi'
+    }
+  }
+]
+
+export default keranjang
